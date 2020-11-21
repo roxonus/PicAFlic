e666649d0de6b6ad8b695df01a777ebad4397439
Jonathan Bower <bowerpower85@gmail.com>
2020-11-21 14:22:24 -0800

    Fixed connection to mongoose DB

---
+++ routes/api.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const Movie = require('../models/movie');
+// Routes
+router.get('/', (req, res) => {
+    Movie.find({  })
+        .then((data) => {
+            console.log('Data: ', data);
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log('error: ', daerrorta);
+        });
+});
+router.post('/save', (req, res) => {
+    const data = req.body;
+    const newMoviePost = new Movie(data);
+    newMoviePost.save((error) => {
+        if (error) {
+            res.status(500).json({ msg: 'Sorry, internal server errors' });
+            return;
+        }
+        // BlogPost
+        return res.json({
+            msg: 'Your data has been saved!!!!!!'
+        });
+    });
+});
+router.get('/name', (req, res) => {
+    const data =  {
+        username: 'peterson',
+        age: 5
+    };
+    res.json(data);
+});
+module.exports = router;
